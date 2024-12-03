29af1706c4c890c46b29851845eb8abc83d2e2a8
Yulduz_Oromova <mahmurbekovna@gmail.com>
2024-12-03 13:17:39 +0500

    First Class

--- src/component/Lids/FirstClass/index.jsx
+++ src/component/Lids/FirstClass/index.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { GenericTable } from '../../Generics/Table';
 import { Action, Container } from './style';
 import { Breadcrumb } from '../../Generics/BreadCrumb';
 import GenericButton from '../../Generics/Button';
-import GenericSelect from '../../Generics/Select';
 import AllLidsModal from './modal';
 import moment from 'moment';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { StudentsContext } from '../../../context/students';
+import GenericInput from '../../Generics/Input';
+import useQuery from '../../../hooks/useQuery';
+import { useNavigate } from 'react-router-dom';
+import useFetch from '../../../hooks/useFetch';
+import replace from "../../../hooks/useReplace"
 
 export const FirstClass = () => {
+  const query = useQuery();
+  const [state, dispatch] = useContext(StudentsContext);
+  const [filter, setFilter] = useState({
+    name: query.get("name") || "",
+    field: query.get("field"),
+    days: query.get("days"),
+    admin: query.get("admin"),
+    date: query.get("date") || "",
+  });
   const [open, setOpen] = useState(false);
   const [modalOpen, setModal] = useState(false);
   const [modalProps, setModalProps] = useState({});
+  const [spinner, setSpinner] = useState(false);
+  const navigate = useNavigate();
+  const request = useFetch();
 
   const onEdit = (e, res) => {
     e.stopPropagation();
@@ -21,13 +38,14 @@ export const FirstClass = () => {
   };
 
   const headCells = [
-    { id: "name", label: "O'quvchining ismi", },
-    { id: "group", label: "Guruh / Fan", },
-    { id: "date", label: "Dars kuni va vaqti", },
-    { id: "addedDate", label: "Qo'shilgan sana", },
-    { id: "admin", label: "Moderator", },
+    { id: "name", label: "O'quvchining ismi" },
+    { id: "field", label: "Guruh / Fan" },
+    { id: "days", label: "Dars kuni va vaqti" },
+    { id: "added_date", label: "Qo'shilgan sana" },
+    { id: "admin", label: "Moderator"},
     {
-      id: "action", label: "",
+      id: "action", 
+      label: "",
       render: (res) => (
         <Action>
           <Action.Edit onClick={(e) => onEdit(e, res)} />
@@ -37,61 +55,38 @@ export const FirstClass = () => {
     },
   ];
 
-  let rows = [
-    {
-      id: 1,
-      name: 'Javlon Javliyev',
-      group: "Frontend",
-      date: "21.05.2024",
-      days: "toq kunlari",
-      addedDate: "21.05.2024",
-      admin: 'Webbrain admin',
-      level: "Beginner"
-    },
-    {
-      id: 2,
-      name: 'Akobir Turdiyev',
-      group: "Frontend",
-      date: "21.05.2024",
-      days: "toq kunlari",
-      addedDate: "21.05.2024",
-      admin: 'Webbrain admin',
-      level: "Junior",
-    },
-    {
-      id: 3,
-      name: 'Oromova Yulduz',
-      group: "Frontend",
-      date: "21.05.2024",
-      days: "toq kunlari",
-      addedDate: "21.05.2024",
-      admin: 'Webbrain admin',
-      level: "Junior",
-    },
-    {
-      id: 4,
-      name: 'Safarova Nilufar',
-      group: "Frontend",
-      date: "21.05.2024",
-      days: "toq kunlari",
-      addedDate: "21.05.2024",
-      admin: 'Webbrain admin',
-      level: "Junior",
-    },
-  ];
-
-  const data1 = [
-    { value: 'uzbek', title: 'Uzbek' },
-    { value: 'russian', title: 'Russian' },
-    { value: 'english', title: 'English' }
-  ];
-
-  const onToggleModal = () => {
+  const onToggleModal = (callback) => {
     setModal(!modalOpen);
     setModalProps(null);
+    callback && callback();
   };
 
   const onSave = () => { };
+
+  const getStudent = async (query = "") => {
+    setSpinner(true);
+    let res = await request(`/tabs/students${query}`);
+    dispatch({ type: "get", payload: Array.isArray(res) ? res : [] });
+    setSpinner(false);
+  };
+
+  const onSelectDate = (event) => {
+    const time = moment(event);
+    let date = `${time.date()}/${time.month()}/${time.year()}`;
+    if (!time.date() && !time.month() && !time.year()) date = null;
+    setFilter({ ...filter, date: 'date' })
+    const query = replace(date, "data");
+    navigate(`${location.pathname}${query}`);
+    getStudent(`/search${query}`);
+  };
+
+  const onChangeFilter = ({ target }) => {
+    const { value, name } = target;
+    setFilter({ ...filter, [name]: value });
+    const query = replace(value, name);
+    navigate(`${location.pathname}${query}`);
+    getStudent(`/search${query}`);
+  };
 
   return (
     <Container>
@@ -100,6 +95,7 @@ export const FirstClass = () => {
         open={modalOpen}
         onClose={onToggleModal}
         onSave={onSave}
+        reload={getStudent}
       />
       <Breadcrumb>
         <GenericButton type='import' onClick={() => setOpen(!open)}>
@@ -109,16 +105,45 @@ export const FirstClass = () => {
           Filter
         </GenericButton>
       </Breadcrumb>
-      <GenericTable open={open} headCells={headCells} rows={rows}>
+      <GenericTable 
+        open={open} 
+        headCells={headCells} 
+        rows={state}
+        spinner={spinner}
+        >
+      <GenericInput
+          value={filter.name}
+          name="name"
+          placeholder="full name"
+          onChange={onChangeFilter}
+        />
+        <GenericInput
+          value={filter.week}
+          name="days"
+          onChange={onChangeFilter}
+          placeholder="hafta kunlari"
+        />
         <LocalizationProvider dateAdapter={AdapterMoment}>
           <DatePicker
-            defaultValue={moment()}
+            value={moment(filter.date)}
+            onChange={onSelectDate}
             views={["year", "month", "day"]}
-            slotProps={{ textField: { size: "small" } }}
+            slotProps={{
+              textField: { size: "small" }
+            }}
+            componentsProps={{
+              actionBar: {
+                actions: ["clear"],
+              },
+            }}
           />
         </LocalizationProvider>
-        <GenericSelect data={data1} />
-        <GenericSelect data={data1} />
+        <GenericInput
+          value={filter.admin}
+          name="admin"
+          onChange={onChangeFilter}
+          placeholder="moderator"
+        />
       </GenericTable>
     </Container>
   );
